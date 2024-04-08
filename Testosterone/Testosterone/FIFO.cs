cb16ca20882433d109948e9d30be0bdeac144b40
Tomáš Celba <celbat22@spstrutnov.com>
2024-04-08 12:25:13 +0200

    pokracovani

--- Testosterone/Testosterone/FIFO.cs
+++ Testosterone/Testosterone/FIFO.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Microsoft.Playwright;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.Security;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Testosterone
 {
-    internal class FIFO
+    public class Tunel
     {
+        private int[] auta;
+        private int pocet = 0;
+
+        public Tunel()
+        {
+            auta = new int[10];
+        }
+
+        public int Count()
+        {
+            return pocet;
+        }
     }
 }
--- Testosterone/Testosterone/UnitTest1.cs
+++ Testosterone/Testosterone/UnitTest1.cs
@@ -3,6 +3,12 @@ namespace Testosterone
     [TestClass]
     public class UnitTest1 
     {
-       
+      [TestMethod]
+      public void tuneljeprazdny()
+        {
+            Tunel tunel = new Tunel();
+
+            Assert.AreEqual(0, Tunel.Count(), "Vytvoøený zásobník není prázdný.");
+        }
     }
 }
