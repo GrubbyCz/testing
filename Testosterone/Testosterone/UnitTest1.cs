82d23affd23a30c584d00d534ef9f97e026e8eb3
Tomáš Celba <celbat22@spstrutnov.com>
2024-04-22 11:26:31 +0200

    zařazování do řady

--- Testosterone/Testosterone/UnitTest1.cs
+++ Testosterone/Testosterone/UnitTest1.cs
@@ -13,5 +13,12 @@ namespace Testosterone
             Assert.AreEqual(0, tunel.Count(), "tunel není prázdný");
 
         }
+        [TestMethod]
+        public void tunelpovlozeniprvkuneniprazdny()
+        {
+            FIFO tunel = new FIFO();
+            tunel.Enqueue(2);
+            Assert.AreNotEqual(0, tunel.Count(), "Tunel je prázdný");
+        }
     }
 }
